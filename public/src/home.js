d89bab8ef12e084f89e8e7f9159c84e2ff6aaaca
Garrett Hu <93103039+GarrettHu1@users.noreply.github.com>
2021-12-28 08:39:49 -0800

    Update home.js

--- public/src/home.js
+++ public/src/home.js
@@ -7,25 +7,54 @@ function getTotalAccountsCount(accounts) {
 }
 
 function getBooksBorrowedCount(books) {
-  //create counter
-  //loop through books
-  //add one to counter each time book is not returned
-  let borrowedBooks = 0 ;
-for (let key of books) {
-if (key.borrows[0].returned === false) {
-borrowedBooks++;
-}
-}
-return borrowedBooks
+  const reducer = (previousValue, currentBook) => {
+    const {borrows} = currentBook
+    if (borrows[0].returned === false) previousValue++
+    return previousValue
+  }
+  const borrowedBooks = books.reduce(reducer, 0)
+  return borrowedBooks
 }
 
 function getMostCommonGenres(books) {
-  
+  let littleHelper = books.map((book) => book.genre);
+  let commonGenres = [];
+  littleHelper.forEach((genre) => {
+    let created = commonGenres.findIndex((genreObj) => genreObj.name === genre);
+    created >= 0
+      ? commonGenres[created].count++
+      : commonGenres.push({ name: genre, count: 1 }); 
+  });
+  return commonGenres.sort((obj1, obj2) => obj2.count - obj1.count).slice(0, 5);
 }
+function getMostPopularBooks(books) {
+  const mostPop = books.map(book => ({name: book.title, count: book.borrows.length}))
 
-function getMostPopularBooks(books) {}
-
-function getMostPopularAuthors(books, authors) {}
+  return mostPop.sort((obj1, obj2) => obj1.count > obj2.count ? -1 : 1 ).slice(0, 5);
+}
+function getMostPopularAuthors(books, authors) {
+  //It returns an array containing five objects or fewer that represents the most popular authors whose books have been checked out the most. 
+  //Popularity is represented by finding all of the books written by the author and then adding up the number of times those books have been borrowed.
+ let authorCounts = {}
+ let authorList =[]
+  for (let author of authors) {
+   for (let book of books) {
+     if (author.id === book.authorId) {
+       let current = authorCounts[`${author.name.first} ${author.name.last}`]
+       if(current) {
+        authorCounts[`${author.name.first} ${author.name.last}`] = current + book.borrows.length
+       }
+       else {
+        authorCounts[`${author.name.first} ${author.name.last}`] = book.borrows.length
+       } 
+     }
+   }
+ }
+for (let item in authorCounts) {
+  authorList.push({name: item, count: authorCounts[item]})
+}
+ return authorList.sort((highCount, lowCount) => lowCount.count - highCount.count).slice(0, 5)
+}
 
 module.exports = {
   getTotalBooksCount,
