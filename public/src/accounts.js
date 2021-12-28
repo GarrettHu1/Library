48643b7a25f39aeede64463c48f652f1b5a18ee6
Garrett Hu <93103039+GarrettHu1@users.noreply.github.com>
2021-12-28 09:00:40 -0800

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -12,9 +12,9 @@ function getTotalNumberOfBorrows(account, books) {
   const {id} = account
   const reducer = (previousValue, currentBook) => {
     const {borrows} = currentBook
-    for (let element of borrows) {
+    borrows.map(element => {
       if (element.id === id) previousValue++
-    }
+    })
     return previousValue
   }
   const totalBorrows = books.reduce(reducer, 0)
@@ -22,12 +22,9 @@ function getTotalNumberOfBorrows(account, books) {
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
-
 const borrowed = books.filter(book => account.id === book.borrows[0].id) ;
 borrowed.forEach(book => book.author = authors.find(author => book.authorId === author.id))
 return borrowed
-
-
 }
 
 
