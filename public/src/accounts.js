dd379615df3e665284076912170dd41e33bd3795
Garrett Hu <93103039+GarrettHu1@users.noreply.github.com>
2021-12-28 08:38:24 -0800

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -9,36 +9,19 @@ return accByLastName
 }
 
 function getTotalNumberOfBorrows(account, books) {
-  let totalBorrows = 0;
-  for (let book of books) {
-const {borrows} = book ;
-for(let i = 0; i < borrows.length; i++) {
-  if(borrows[i].id === account.id) {
-    totalBorrows++
+  const {id} = account
+  const reducer = (previousValue, currentBook) => {
+    const {borrows} = currentBook
+    for (let element of borrows) {
+      if (element.id === id) previousValue++
+    }
+    return previousValue
   }
-}
-  }
+  const totalBorrows = books.reduce(reducer, 0)
   return totalBorrows
 }
 
 function getBooksPossessedByAccount(account, books, authors) {
-  //loop through books 
-  //find books checked out with id matching account
-  //push book into empty array
-  //
-  //booksFiltered.index.author =  authors[authorId].name
-
-/*  
-const booksFiltered = books.filter((book) => book.borrows[0].returned === false && book.borrows[0].id === account.id) 
-for (let book of booksFiltered) {
-  for (let author of authors) {
-    if (book.authorId === author.id) {
-      book.author = author.name
-    }
-  }
-  return booksFiltered
-}
-*/
 
 const borrowed = books.filter(book => account.id === book.borrows[0].id) ;
 borrowed.forEach(book => book.author = authors.find(author => book.authorId === author.id))
